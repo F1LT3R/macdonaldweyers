cf3866fa99b4255628939860f8bc8c1aafc70fd4
Alistair MacDonald <amacdonald@aisquared.com>
2015-01-05 21:12:40 -0500

    Fix map.

--- map.js
+++ map.js
@@ -5,15 +5,6 @@ var coords = [42.3569, -71.0622];
 var map = L.mapbox.map('map', 'examples.map-i86nkdio')
     .setView(coords, 15);
 
-// var marker = L.marker(coords, {
-//     icon: L.mapbox.marker.icon({
-//       'marker-color': '#f86767'
-//     })
-// })
-// .addTo(map)
-// ;
-
-
 
 L.mapbox.featureLayer({
     type: 'Feature',
@@ -23,12 +14,12 @@ L.mapbox.featureLayer({
     },
     properties: {
         title: 'Park Street Church',
-        description: '1 Park Street, Boston, MA 02108, (617) 523-3383',
-        // one can customize markers by adding simplestyle properties
-        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+        description: '1 Park Street, Boston, MA 02108 <br> (617) 523-3383',
         'marker-size': 'large',
         'marker-color': '#BE9A6B',
-        // 'marker-symbol': 'religious-christian'
         'marker-symbol': 'religious-christian'
     }
 }).addTo(map);
+
+
+map.scrollWheelZoom.disable();
